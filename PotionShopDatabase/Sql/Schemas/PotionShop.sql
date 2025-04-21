0e40e354d98564111f8206b0a2b30fcdecf8e967
Nathan Proctor <nproctor@ksu.edu>
2025-04-21 12:34:55 -0500

    added a little bit to the sql

---
+++ PotionShopDatabase/Sql/Schemas/PotionShop.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS
+	(
+		SELECT * 
+		FROM sys.schemas s
+		WHERE s.[name] = N'PotionShop'
+	)
+BEGIN
+	EXEC(N'CREATE SCHEMA [PotionShop] AUTHORIZATION [dbo]');
+END;
---
+++ PotionShopDatabase/Sql/Tables/PotionShop.Employee.sql
@@ -0,0 +1,22 @@
+﻿IF OBJECT_ID(N'PotionShop.Employee') IS NULL
+BEGIN
+	CREATE TABLE PotionShop.Employee
+	(
+		EmployeeID INT NOT NULL,
+		StoreID INT NOT NULL,
+		FirstName NVARCHAR(32) NOT NULL,
+		LastName NVARCHAR(32) NOT NULL,
+		EmployeeHours NVARCHAR(64) NOT NULL,
+		Salary INT NOT NULL,
+		Position NVARCHAR(32) NOT NULL,
+		GoldStars INT NOT NULL DEFAULT(0),
+
+		CONSTRAINT PK_PotionShop_Employee_EmployeeID PRIMARY KEY CLUSTERED
+		(
+			EmployeeID ASC
+		),
+
+		CONSTRAINT [FK_PotionShop_Employee_PotionShop_Store] FOREIGN KEY(StoreID)
+		REFERENCES PotionShop.Store(StoreID)
+	);
+END;
