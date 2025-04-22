d2cb7e364233128a2439e4218dd9009e7ce79d34
Zacklatta <zalatta@ksu.edu>
2025-04-22 14:34:10 -0500

    Added StoreItem Stuff

--- EmployeeDataTests/SqlEmployeeRepositoryTest.cs
+++ EmployeeDataTests/SqlEmployeeRepositoryTest.cs
@@ -7,7 +7,7 @@ namespace PotionShopDatabase.Tests
     [TestClass]
     public class SqlEmployeeRepositoryTest
     {
-        const string connectionString = @"Server=(localdb)\MSSQLLocalDb;Database=nathanproctor;Integrated Security=SSPI;"; //PLEASE CHANGE THE Database=johnkeller TO WHATEVER YOU MADE IT IN RebuildDatabase.ps1
+        const string connectionString = @"Server=(localdb)\MSSQLLocalDb;Database=zacklatta;Integrated Security=SSPI;"; //PLEASE CHANGE THE Database=johnkeller TO WHATEVER YOU MADE IT IN RebuildDatabase.ps1
 
         private static string GetTestString() => Guid.NewGuid().ToString("N");
 
