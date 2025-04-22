f7fc90df566db1fb0a3dd7d6d6c7a941c8c173df
Nathan Proctor <nproctor@ksu.edu>
2025-04-22 12:16:50 -0500

    Going to work on retrieving an employee

---
+++ PotionShopDatabase/DataDelegates/CreateEmployeeDataDelegate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using PotionShopDatabase.Models;
+using System.Data;
+
+
+namespace PotionShopDatabase.DataDelegates
+{
+    internal class CreateEmployeeDataDelegate(int storeID, string firstName, string lastName, string employeeHours, int salary, string position, int goldStars)
+        : NonQueryDataDelegate<Employee>("Employee.CreateEmployee")
+
+    {//In his project he has some that are private readonly but some arent and they are just plugged in elsewhere. Ill plug in elsewhere for now
+        /*
+        private readonly string firstName = firstName;      
+        private readonly string lastName = lastName;
+        */
+        public override void PrepareCommand(Command command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("StoreID", storeID);
+            command.Parameters.AddWithValue("FirstName", firstName);
+            command.Parameters.AddWithValue("LastName", lastName);
+            command.Parameters.AddWithValue("EmployeeHours", employeeHours);
+            command.Parameters.AddWithValue("Salary", salary);
+            command.Parameters.AddWithValue("Position", position);
+            command.Parameters.AddWithValue("GoldStars", goldStars);
+
+            var p = command.Parameters.Add("EmployeeID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+        }
+
+        public override Employee Translate(Command command)
+        {
+            return new Employee(command.GetParameterValue<int>("EmployeeID"), storeID, firstName, lastName, employeeHours, salary, position, goldStars);
+        }
+
+    }
+}
---
+++ PotionShopDatabase/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShopDatabase.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; }
+        public int StoreID { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string EmployeeHours { get; }
+        public int Salary { get; }
+        public string Position { get; }
+        public int GoldStars { get; }
+
+        internal Employee(int employeeID, int storeID, string firstName, string lastName, string employeeHours,
+            int salary, string position, int goldStars)
+        {
+            EmployeeID = employeeID;
+            StoreID = storeID;
+            FirstName = firstName;
+            LastName = lastName;
+            EmployeeHours = employeeHours;
+            Salary = salary;
+            Position = position;
+            GoldStars = goldStars;
+        }
+    }
+}
