bd1983000ebf635ae29e7eec7ada02f233943c33
Daniel Cortez <cordan231@gmail.com>
2025-04-22 14:52:46 -0500

    Added Order model

--- PotionShopDatabase/Models/Order.cs
+++ PotionShopDatabase/Models/Order.cs
@@ -8,5 +8,16 @@ namespace PotionShopDatabase.Models
 {
     public class Order
     {
+        public int OrderID { get; }
+        public int StoreID { get; }
+        public DateTime OrderedOn { get; }
+
+        internal Order(int orderID, int storeID, DateTime orderedOn)
+        {
+            this.OrderID = orderID;
+            this.StoreID = storeID;
+            this.OrderedOn = orderedOn;
+        }
+
     }
 }
