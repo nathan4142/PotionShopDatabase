6c7d447cf5278bdfc0de86a42d3fb60ad1a84ffd
Zacklatta <zalatta@ksu.edu>
2025-04-23 13:32:19 -0500

    what is happening

---
+++ PotionShopDatabase/Models/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShopDatabase.Models
+{
+    public class Item
+    {
+        public int ItemID { get; }
+
+        public string Name { get; }
+
+        public string ItemDescription { get; }
+
+        public List<string> Ingredients { get; }
+
+        public decimal Price { get; }
+
+        public PotionType PotionType { get; }
+
+        public Item(int i, string n, string id, List<string> ingredients, decimal p, PotionType pt)
+        {
+            ItemID = i;
+            Name = n;
+            ItemDescription = id;
+            Ingredients = ingredients;
+            Price = p;
+            PotionType = pt;
+        }
+
+
+    }
+}
---
+++ PotionShopDatabase/Models/PotionType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShopDatabase.Models
+{
+    public enum PotionType
+    {
+        Enhance = 1,
+        Health = 2,
+        Skill = 3,
+        Effect = 4
+
+    }
+}
