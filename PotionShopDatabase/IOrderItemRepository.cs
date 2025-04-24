a09175ec6076c47bb5fc816d15557757a980c91e
Daniel Cortez <cordan231@gmail.com>
2025-04-24 17:33:18 -0500

    Finished delegates and repositories for order item

---
+++ PotionShopDatabase/IOrderItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using PotionShopDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotionShopDatabase
+{
+    internal interface IOrderItemRepository
+    {
+        OrderItem CreateOrderItem(int orderID, int storeItemID, int itemQuantity);
+    }
+}
---
+++ PotionShopDatabase/SqlOrderItemRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using PotionShopDatabase.Models;
+using PotionShopDatabase.DataDelegates;
+
+namespace PotionShopDatabase
+{
+    public class SqlOrderItemRepository(string connectionString) : IOrderItemRepository
+    {
+        private readonly SqlCommandExecutor executor = new(connectionString);
+
+        public OrderItem CreateOrderItem(int orderID, int storeItemID, int itemQuantity)
+        {
+            
+            ArgumentException.ThrowIfNullOrWhiteSpace(Convert.ToString(orderID));
+            ArgumentException.ThrowIfNullOrWhiteSpace(Convert.ToString(storeItemID));
+            ArgumentException.ThrowIfNullOrWhiteSpace(Convert.ToString(itemQuantity));
+
+            var d = new CreateOrderItemDataDelegate(orderID, storeItemID, itemQuantity);
+            return executor.ExecuteNonQuery(d);
+        }
+    }
+}
