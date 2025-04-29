a87f72cb6e87365afcf2df67fe7ab1c7865985ad
Daniel Cortez <cordan231@gmail.com>
2025-04-29 11:59:19 -0500

    Committing to pull OrderStoreItem

--- SQLUserInterface/StoreTable.cs
+++ SQLUserInterface/StoreTable.cs
@@ -26,7 +26,7 @@ namespace SQLUserInterface
             dataTable.Columns.Add("ZipCode");
 
             //Goes to the repository which is where we will call the methods from
-            var repo = new SqlStoreRepository(@"Server=(localdb)\MSSQLLocalDb;Database=nathanproctor;Integrated Security=SSPI;");
+            var repo = new SqlStoreRepository(@"Server=(localdb)\MSSQLLocalDb;Database=danielcortez;Integrated Security=SSPI;");
             //Calls the get all stores method and stores it in the stores variable
             var stores = repo.GetAllStores();
             //For each of the stores in stores, we add it to the dataTable
@@ -61,7 +61,7 @@ namespace SQLUserInterface
             dataTable.Columns.Add("ZipCode");
             dataTable.Columns.Add("TotalGoldStars");
 
-            var repo = new SqlStoreRepository(@"Server=(localdb)\MSSQLLocalDb;Database=nathanproctor;Integrated Security=SSPI;");
+            var repo = new SqlStoreRepository(@"Server=(localdb)\MSSQLLocalDb;Database=danielcortez;Integrated Security=SSPI;");
             int goldStars = (int)ux_numGoldStars.Value;
             var coolestStores = repo.GetCoolestStores(goldStars);
 
