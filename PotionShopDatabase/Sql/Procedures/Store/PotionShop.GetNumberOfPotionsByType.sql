ec280bf12f379a850eda563f3dc0490f212c15fc
Daniel Cortez <cordan231@gmail.com>
2025-04-30 16:36:06 -0500

    SQL Code for getting number of potions in store by type

---
+++ PotionShopDatabase/Sql/Procedures/Store/PotionShop.GetNumberOfPotionsByType.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR ALTER PROCEDURE PotionShop.GetNumberOfPotionsByType
+	@PotionTypeID INT
+AS
+BEGIN
+
+SELECT
+  S.StoreID,
+  S.[Address],
+  S.StateCode,
+  S.ZipCode,
+  COALESCE(SUM(SI.Quantity),0) AS PotionCount
+FROM PotionShop.Store S
+  LEFT JOIN PotionShop.StoreItem SI ON SI.StoreID = S.StoreID
+  LEFT JOIN PotionShop.Item I ON I.ItemID = SI.ItemID AND I.PotionTypeID = @PotionTypeID
+GROUP BY S.StoreID, S.[Address], S.StateCode, S.ZipCode
+ORDER BY S.StoreID;
+
+END;
